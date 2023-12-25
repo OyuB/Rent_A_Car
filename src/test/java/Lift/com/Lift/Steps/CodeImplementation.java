49c3f713365e07a4a6dd5c134078451c455af64f
OyuBuren <obmglub@gmail.com>
2023-12-25 09:47:10 -0600

    Gigster example push

--- src/test/java/Lift/com/Lift/Steps/CodeImplementation.java
+++ src/test/java/Lift/com/Lift/Steps/CodeImplementation.java
@@ -5,6 +5,7 @@ import io.cucumber.java.en.Given;
 public class CodeImplementation {
 
    //Actual codding logic tests written here
-    @Given("I logged Library api as a {string}")
-    public void i_logged_library_api_as_a(String librarian) {}
+   @Given("I logged Lift api as a {string}")
+   public void i_logged_lift_api_as_a(String rider) {}
+
 }
